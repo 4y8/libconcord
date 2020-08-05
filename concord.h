7d6cd750f3f6daa9be0a926c6f884bb6b4a8dd0d
Yul3n <yul3n.falx@protonmail.com>
2020-08-05 15:08:11 +0200

    Initial commit.

--- concord.h
+++ concord.h
@@ -1,5 +1,7 @@
 typedef struct {
 		char *content;
+		int id;
+		int channel_id;
 } concord_message;
 
 typedef struct {
@@ -8,7 +10,10 @@ typedef struct {
 		char *login;
 } concord_client;
 
+char *concord_discord_base_url = "https://discord.com/api/v6";
+
 static void concord_reply(char *content, concord_message *msg);
 static void concord_login(concord_client *client, char *token);
 static void concord_free_client(concord_client *client);
+static concord_message *concord_get_channel_messages(int channel_id);
 static concord_client *concord_new_client();
--- examples/ping.c
+++ examples/ping.c
@@ -1,21 +1,22 @@
-#include "../concord.h"
 #include <string.h>
+#include "../concord.h"
+
 #define TOKEN ""
 
 void
 pong(concord_message *msg)
 {
 
-    if (!strcmp("ping", msg->content)) concord_reply("pong", msg);
+        if (!strcmp("ping", msg->content)) concord_reply("pong", msg);
 }
 
 int
 main()
 {
-    concord_client *bot;
+        concord_client *bot;
 
-    bot = concord_new_client();
-    concord_login(bot, TOKEN);
-    bot->message_callback = pong;
-    concord_free_client(bot);
+        bot = concord_new_client();
+        concord_login(bot, TOKEN);
+        bot->message_callback = pong;
+        concord_free_client(bot);
 }
