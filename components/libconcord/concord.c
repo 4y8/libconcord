616976ed9a375ba94dab36774e1d19199d82e218
Yul3n <yul3n.falx@protonmail.com>
2020-08-06 11:59:17 +0200

    Continued.

--- components/libconcord/concord.c
+++ components/libconcord/concord.c
@@ -1,9 +1,36 @@
 #include <stdlib.h>
+#include "esp_http_client.h"
 #include "include/concord.h"
 
-void
+static void
+concord_reply(char *content, concord_message *msg)
+{
+   
+}
+
+static void
+concord_login(concord_client *client, char *token)
+{
+   
+}
+
+static concord_message *
+concord_get_channel_messages(int channel_id)
+{
+
+        return NULL;
+}
+
+static void
 concord_free_client(concord_client *client)
 {
 
-    free(client);
+        free(client);
+}
+
+static concord_client *
+concord_new_client()
+{
+
+        return NULL;
 }
